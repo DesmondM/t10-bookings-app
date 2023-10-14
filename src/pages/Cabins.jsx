e56fa454658eb97cf5396e81f1460d96dab5b1e7
Desmond <desmondmpofu@gmail.com>
2023-10-14 12:03:42 +0200

    data fetching

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -2,14 +2,20 @@ import { useEffect } from "react";
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import { getRooms } from "../services/apiRooms";
+import CabinTable from "../features/cabins/CabinTable";
 
 function Cabins() {
   
   return (
+    <>
     <Row type="horizontal">
       <Heading as="h1">All cabins</Heading>
-      <p>TEST</p>
+      <p>Filter / Sort</p>
      </Row>
+     <Row>
+        <CabinTable />
+     </Row>
+     </>
   );
 }
 
